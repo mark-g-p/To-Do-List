f596af72f048dcf7aae14b5307507ac37379fa3d
Marek Przybylowicz <marek.g.przybylowicz@gmail.com>
2019-06-27 11:35:53 +0200

    Redirect after POST. Show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -56,7 +56,6 @@ class NewVisitorTest(unittest.TestCase):
         #Edith wonders whether the site will remember her list. Then she sees
         #that the site has generated a unique URL for her -- there is some
         #explanatory text.
-        self.fail('Finish the test!')
         
         #She visits that URL - her to-do list is still there.
         
