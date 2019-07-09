ecdd8ff08069962667808f43f217e84c5061cc41
Marek Przybylowicz <marek.g.przybylowicz@gmail.com>
2019-07-09 10:21:28 +0200

    Replace voodoo sleeps with something proper

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 import time
-import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,11 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table=self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about new online to-do list app. 
         #She goes to check out its homepage
@@ -40,20 +51,18 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
 
         #There is still a text box inviting her to add another item. 
         #She enters "Use peacock feathers"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
     
         #The page updates again, and now she shows both items on her list
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers')
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('2. Use peacock feathers')
         
         #Edith wonders whether the site will remember her list. Then she sees
         #that the site has generated a unique URL for her -- there is some
@@ -62,4 +71,4 @@ class NewVisitorTest(LiveServerTestCase):
         #She visits that URL - her to-do list is still there.
         
         #Satisified, she leaves page
-        slef.fail('Finish the test!')        
+        self.fail('Finish the test!')        
