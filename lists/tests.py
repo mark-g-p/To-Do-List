916bed82866901d6ec5dcf96ec809bfb32e13e17
Marek Przybylowicz <marek.g.przybylowicz@gmail.com>
2019-07-16 15:07:28 +0200

    Add model for lists and change item model.

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,26 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
-class ItemModelTest(TestCase):    
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
-        first_item = Item.objects.create(text='The first (ever) list item')     
-        second_item = Item.objects.create(text = 'Item the second')
+        item_list = List()
+        item_list.save()
+
+        first_item = Item.objects.create(text='The first (ever) list item', list = item_list)     
+        second_item = Item.objects.create(text = 'Item the second', list = item_list)
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, item_list)
 
         self.assertEqual(Item.objects.count(), 2)
 
         first_saved_item = Item.objects.get(pk=1)
         second_saved_item = Item.objects.get(pk=2)
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, item_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
-
+        self.assertEqual(second_saved_item.list, item_list)
 
 class HomePageTest(TestCase):
    
@@ -29,8 +36,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'lists/list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        item_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=item_list)
+        Item.objects.create(text='itemey 2', list=item_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request,'lists/home.html')
@@ -9,5 +9,6 @@ def view_list(request):
     return render(request, 'lists/list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    item_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=item_list)
     return redirect('/lists/the-only-list-in-the-world/') 
