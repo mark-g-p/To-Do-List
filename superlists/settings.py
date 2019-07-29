6f5e4906feaccd1de9dec02e49c95835c9f8c7ce
marek.g.przybylowicz <marek.g.przybylowicz@gmail.com>
2019-07-29 17:47:40 +0200

    Fix typo in template and settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4uknwc&ldyvl6dqgr95pq(#%!x*$wq8dvtkwm%779=dy5wb%54'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
