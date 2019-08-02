273d1e752d56e63788d0d23a0e530c82c00d92bf
marek.g.przybylowicz <marek.g.przybylowicz@gmail.com>
2019-08-02 08:58:22 +0200

    Fix typo in functional tests files names

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,20 +1,95 @@
-from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-class ItemValidationTest(FunctionalTest):
 
-    def test_cannot_add_empty_list_items(self):
-          # Edith goes to the home page and accidentally tries to submit
-          # an empty list item. She hits Enter on the empty input box
-      
-          # The home page refreshes, and there is an error message saying
-          # that list items cannot be blank
-      
-          # She tries again with some text for the item, which now works
-      
-          # Perversely, she now decides to submit a second blank list item
-      
-          # She receives a similar warning on the list page
-      
-          # And she can correct it by filling some text in
-      self.fail('write me!')
+class NewVisitorTest(FunctionalTest):
+
+    def test_can_start_a_list_for_one_user(self):
+        #Edith has heard about new online to-do list app.
+        #She goes to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        #She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        #She types "Buy peacock feathers" into a text box.
+        inputbox.send_keys('Buy peacock feathers')
+
+        #When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+
+        #There is still a text box inviting her to add another item.
+        #She enters "Use peacock feathers"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+
+        #The page updates again, and now she shows both items on her list
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('2. Use peacock feathers')
+
+        #Edith wonders whether the site will remember her list. Then she sees
+        #that the site has generated a unique URL for her -- there is some
+        #explanatory text.
+
+        #She visits that URL - her to-do list is still there.
+
+        #Satisified, she leaves page
+
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+
+        # SHe notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis, comes to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers', page_text)
+
+        # Francis starts new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
