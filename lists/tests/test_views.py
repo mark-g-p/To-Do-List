4d3b77012bf3ae03148ebb122be4969a4d6082a0
marek.g.przybylowicz <marek.g.przybylowicz@gmail.com>
2019-08-13 20:55:14 +0200

    Use new form in home_page, simplify tests, prepare to fix attributes

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils.html import escape
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -8,6 +9,9 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
 
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
+from lists.forms import ItemForm
 from lists.models import Item, List
-# TO-DO Remove hardcoded URLs
+
 def home_page(request):
-    return render(request,'lists/home.html')
+    return render(request,'lists/home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
     item_list = List.objects.get(id=list_id)
