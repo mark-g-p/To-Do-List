03254438f5f58ac86d6b2b0c140cefe623e5e13d
marek.g.przybylowicz <marek.g.przybylowicz@gmail.com>
2019-08-19 16:46:17 +0200

    Fix the dumb typo in the unit test

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
 from lists.models import Item, List
-from unittest import skip 
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -78,7 +81,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -87,15 +90,14 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         item_list = List.objects.create()
         response = self.client.get(f'/lists/{item_list.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
-   
-    @skip("waiting for implementation")   
+
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='testin')
-        response = self.client.post(f'/list/{list1.id}/', data={'text': 'testin'})
-        expected_error = escape("You've already got this in your list")
+        response = self.client.post(f'/lists/{list1.id}/', data={'text': 'testin'})
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'lists/list.html')
         self.assertEqual(Item.objects.all().count(), 1)
