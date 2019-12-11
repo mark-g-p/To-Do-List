19c34e38fa3ac0c18a7a385363a3da01d9fa0b59
marek.g.przybylowicz <marek.g.przybylowicz@gmail.com>
2019-12-11 14:11:20 +0100

    Write isolated tests for forms

--- lists/forms.py
+++ lists/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from lists.models import Item
+from lists.models import Item, List
 
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -23,8 +24,16 @@ class ItemForm(forms.ModelForm):
         self.instance.list = for_list
         return super().save()
 
-class NewListForm(object):
-    pass
+
+class NewListForm(ItemForm):
+
+    def save(self, owner):
+        if owner.is_authenticated:
+            List.create_new(
+                first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            List.create_new(first_item_text=self.cleaned_data['text'])
+
 
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,19 +1,25 @@
+import unittest
+
+from unittest.mock import patch, Mock
 from django.test import TestCase
+
 from lists.forms import (
-        DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, 
-        ExistingListItemForm, ItemForm
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm, NewListForm
 )
 from lists.models import Item, List
 
+
+
 class ItemFormTest(TestCase):
-    
+
     def test_form_item_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
-        form = ItemForm(data={'text':''})
+        form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['text'],
@@ -28,8 +34,9 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.text, 'Do me')
         self.assertEqual(new_item.list, item_list)
 
+
 class ExistingListItemFormTest(TestCase):
-    
+
     def test_form_renders_item_text_input(self):
         item_list = List.objects.create()
         form = ExistingListItemForm(for_list=item_list)
@@ -44,12 +51,42 @@ class ExistingListItemFormTest(TestCase):
     def test_form_validation_for_duplicate_items(self):
         item_list = List.objects.create()
         Item.objects.create(list=item_list, text='no twins')
-        form = ExistingListItemForm(for_list=item_list, data={'text': 'no twins'})
+        form = ExistingListItemForm(
+            for_list=item_list, data={'text': 'no twins'})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['text'],[DUPLICATE_ITEM_ERROR])
-    
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
     def test_form_save(self):
         item_list = List.objects.create()
         form = ExistingListItemForm(for_list=item_list, data={'text': 'hi'})
         new_item = form.save()
         self.assertEqual(new_item, Item.objects.all()[0])
+
+
+class NewListFormTest(unittest.TestCase):
+
+    @patch('lists.forms.List.create_new')
+    def test_save_creates_new_list_from_post_data_if_user_not_authenticated(
+        self,
+        mock_List_create_new
+    ):
+        user = Mock(is_authenticated=False)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text="new item text"
+        )
+
+    @patch('lists.forms.List.create_new')
+    def test_save_creates_new_list_with_owner_if_authenticated(
+        self,
+        mock_List_create_new
+    ):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text="new item text", owner=user
+        )
